45ba76e988390a239d9f127604fce8d9f297b841
France Malele <f.malele24@gmail.com>
2025-06-23 16:48:21 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
+      <h1>🎧 Welcome to BeatMixStudios</h1>
+      <p>For Creators, For Learners, For Dreamers</p>
+      <p>Phase 1 Preview Active</p>
+    </div>
+  );
+}
