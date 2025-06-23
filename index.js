9e54d7556b2331c3222f5c8ca8bfa3f69d90f3a8
France Malele <f.malele24@gmail.com>
2025-06-23 20:03:52 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '5rem' }}>
-      <h1>🎧 Welcome to BeatMixStudios</h1>
-      <p>For Creators, For Learners, For Dreamers</p>
-      <p>Phase 1 Preview Active</p>
-    </div>
-  );
-}
